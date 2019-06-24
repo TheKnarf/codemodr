04ab036a9830266695ea2784809fdbc04a94937d
Frank Lyder Bredland <fl@nyggen.com>
2019-06-24 18:07:13 +0200

    Beginning of a Codemod to convert code to use css modules

---
+++ src/css-module.js
@@ -0,0 +1,45 @@
+export default function transformer(file, api) {
+	const j = api.jscodeshift,
+        root = j(file.source);
+  //const babelTypes = require('babel-types');
+
+  root
+		.find(j.ImportDeclaration)
+		.forEach(path => {
+			const importV = path.value.source.value;
+
+			if(/\.css$/.test(importV) && !/\.module\.css$/.test(importV)) {
+        const newName = importV.substring(0, importV.length - 4) + '.module.css';
+        j(path).replaceWith(`import css from '${newName}';`)
+			}
+		})
+
+  const fixClassName = oldClassName => oldClassName
+    .replace(/\-+./gi, x => x.substring(x.length-1).toUpperCase());
+
+  root
+    .find(j.JSXAttribute)
+    .filter(path => path.value.name.name == 'className')
+    .forEach(path => {
+      j(path)
+        .find(j.StringLiteral)
+        .forEach(path => {
+          if(!/^\ *$/.test(path.value.value)) {
+            const newClassName = path.value.value
+              .split(' ')
+              .map(fixClassName)
+              .map(name => `css.${name}`);
+
+            if(newClassName.length == 1) {
+              j(path).replaceWith(`{${newClassName[0]}}`);
+            } else {
+              const arr = newClassName.join(',');
+              j(path).replaceWith(`{[${arr}].join(' ')}`);;
+            }
+          }
+        });
+    });
+
+  return root.toSource();
+}
+export const parser = 'tsx';
